30e22152ef1c6103d445605da99ee80f0f83490e
MrAlecJohnson <alecijohnson@gmail.com>
2019-10-23 14:01:59 +0100

    Fixed an error on book counting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,13 @@ class HomeController < ApplicationController
         @this_year = Book.where(:finished => Date.new(today.year,1,1)..today)
         @this_month_count = @this_year.where(:finished => Date.new(today.year,today.month,1)..today).count 
         @this_year_count = @this_year.count
-        @women_this_year = (100*@this_year.where(:sex == "F").count/@this_year_count).round
+        
+        if @this_year_count > 0
+            @women_this_year = (100*@this_year.where(:sex == "F").count/@this_year_count).round
+        else
+            @women_this_year = 0
+        end
+
         @all_time_count = Book.count
 
     end
